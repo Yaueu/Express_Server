3d12f9706fd0f5c99ab9cf70bac3dc03bb69c3ed
Lancaster <lancaster_84@mail.ru>
2019-02-15 21:16:25 +0300

    Чуток почистил от "пока лишнего"

--- index.js
+++ index.js
@@ -2,20 +2,17 @@
 const express = require ('express');
 const app = express ();
 // Обработка параметров ссылки
-app.get('/index', function (req, res, next) {
+app.get('/index', function (req, res) {
     res.send("Главная страница /index");
-    next;
 });
-app.get('/about', function (req, res, next) {
+app.get('/about', function (req, res) {
     res.send("Страница описания /about");
-    next;
 });
-app.get('/settings', function (req, res, next) {
+app.get('/settings', function (req, res) {
     res.send("Страница настроек /settings");
-    next;
 });
 app.get('/settings/:messageId', function(req,res){
     // Вывод сообщения на страницу
     res.send('<h1>Страница настроек /settings</h1>' + 'Привет ' + req.params['messageId'])
 });
-app.listen(8080)
+app.listen(8080);
